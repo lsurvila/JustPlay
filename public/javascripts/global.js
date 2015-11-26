f717626c3357fe699f6ab72c7b85a1c45ab32a3d
Liudas Survila <liudas.survila@gmail.com>
2015-11-26 11:07:31 +0000

    - wip on file download (looks like you can't do that with jquery without any workarounds, looking for alternatives)

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,6 +1,7 @@
-$('#buttonDownload').on('click', downloadTestVideo);
+$('#buttonDownloadServer').on('click', downloadVideoServer);
+$('#buttonDownloadClient').on('click', downloadVideoClient);
 
-function downloadTestVideo(event) {
+function downloadVideoServer(event) {
     event.preventDefault();
     $.ajax({
         type: 'GET',
@@ -8,6 +9,17 @@ function downloadTestVideo(event) {
         success: function(response) {
             alert('Success: ' + response + ' video downloaded');
         },
+        error: function(request, status, error) {
+            alert("Error");
+        }
+    });
+}
+
+function downloadVideoClient(event) {
+    event.preventDefault();
+    $.ajax({
+        type: 'GET',
+        url: 'video/download-to-client',
         error: function(request, status, error) {
             alert("Error");
         }
--- routes/video.js
+++ routes/video.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 router.get('/download-to-server', function(req, res) {
     downloadTestVideoToServer(res);
-    //res.send((err === null) ? {msg: 'success'} : {msg: err});
+});
+
+router.get('/download-to-client', function(req, res) {
+    var path = require('path');
+    var fs = require('fs');
+    var youtubedl = require('youtube-dl');
+    var video = youtubedl('5LG2Ar2ny0M');
+
+
+    video.on('error', function (error) {
+        res.status(400).send(error.message);
+    });
+
+    video.on('info', function (info) {
+        console.log('Download started');
+        console.log('filename: ' + info.filename);
+        console.log('size; ' + info.size);
+        var output = path.join('data', 'videos', info.filename + '.mp4');
+        video.pipe(fs.createWriteStream(output));
+        console.log('sending file');
+        res.download('/Users/liudassurvila/Documents/WebStormProjects/JustPlay/' + output);
+        console.log('send file complete');
+    });
 });
 
 function downloadTestVideoToServer(res) {
@@ -14,7 +36,7 @@ function downloadTestVideoToServer(res) {
 
 
     video.on('error', function (error) {
-        res.status(500).send(error.message);
+        res.status(400).send(error.message);
     });
 
     video.on('info', function (info) {
