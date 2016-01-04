b624af6536b2d226186867964aa7af286f06ed66
Liudas Survila <liudas.survila@gmail.com>
2016-01-04 12:16:30 +0000

    - removed home page

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
 var users = require('./routes/users');
 var video = require('./routes/video');
 
@@ -27,7 +26,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 app.use('/users', users);
 app.use('/video', video);
 
